93ea031a96b7037f7d3dacc46465447cacbfec33
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2019-07-19 08:59:25 +0200

    Improve configuration

--- rjsconfig-client.js
+++ rjsconfig-client.js
@@ -2,13 +2,16 @@
     generateSourceMaps: true,
     name: "@ordbok/plugin-boilerplate",
     out: "dist/client.js",
+    exclude: [
+        "@ordbok/core"
+    ],
     packages: [{
+        name: "@ordbok/core",
+        main: "index",
+        location: 'node_modules/@ordbok/core/dist/client'
+    }, {
         name: "@ordbok/plugin-boilerplate",
         main: "index",
         location: 'dist/client'
-    }, {
-        name: "@ordbok/core",
-        main: "index",
-        location: "node_modules/@ordbok/core/dist/client"
     }]
 })
